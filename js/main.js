7e2651779e2c419faa4e854bdc16a4293fb3d3b9
Zaur <zaurdysh@gmail.com>
2022-10-26 18:28:55 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(".info__video-link").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false,
+  });
+
+  $(".gallery__content").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1],
+    },
+  });
+});
+
+$(".menu-btn").on("click", function () {
+  $(".header__list").toggleClass("active");
+})
+
